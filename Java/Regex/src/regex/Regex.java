76c951f75bc4c31682057316dd37cdaef7572398
Ark223 <Ark223Neww@gmail.com>
2019-12-10 20:39:35 +0100

    R

---
+++ Java/Regex/src/regex/Regex.java
@@ -0,0 +1,22 @@
+
+package regex;
+
+import java.util.*;
+import java.util.regex.*;
+
+public class Regex
+{
+    public static void main(String[] args) { }
+    
+    public static boolean ValidateZipCode(String text) {
+        return text.matches("^[0-9]{2}(?:-[0-9]{3})?$");
+    }
+    
+    public static List<String> FindEmails(String text) {
+        Pattern p = Pattern.compile("([a-z0-9_.-]+)@([a-z0-9_.-]+[a-z])");
+        Matcher m = p.matcher(text);
+        List<String> results = new ArrayList<>();
+        while (m.find()) results.add(m.group());
+        return results;
+    }
+}
---
+++ Java/Regex/test/regex/RegexTest.java
@@ -0,0 +1,49 @@
+
+package regex;
+
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class RegexTest {
+    
+    public RegexTest() { TestFuncs(); }
+    
+    @Test
+    public void TestFuncs() {
+        System.out.println("Test walidacji email:");
+        
+        System.out.println("1.");
+        List<String> res1 = Regex.FindEmails("bartosz@wp.pl[,darek23@gmail.com[");
+        List<String> exp1 = Arrays.asList("bartosz@wp.pl", "darek23@gmail.com");
+        assertEquals(res1, exp1);
+        
+        System.out.println("2.");
+        res1 = Regex.FindEmails("bartosz@wp.pl[,darek23gmail.com[");
+        exp1 = Arrays.asList("bartosz@wp.pl");
+        assertEquals(res1, exp1);
+        
+        System.out.println("3.");
+        res1 = Regex.FindEmails("bartosz@wp.pl[,darek23gmailcom[");
+        exp1 = Arrays.asList("bartosz@wp.pl");
+        assertEquals(res1, exp1);
+        
+        System.out.println("Test walidacji kodow pocztowych:");
+        
+        System.out.println("1.");
+        boolean res2 = Regex.ValidateZipCode("26872");
+        boolean exp2 = false;
+        assertEquals(res2, exp2);
+        
+        System.out.println("2.");
+        res2 = Regex.ValidateZipCode("26-872");
+        exp2 = true;
+        assertEquals(res2, exp2);
+        
+        System.out.println("3.");
+        res2 = Regex.ValidateZipCode("26-8726");
+        exp2 = false;
+        assertEquals(res2, exp2);
+    }
+}
