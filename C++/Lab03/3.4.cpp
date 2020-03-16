05fe2c713f47aa4e388ed14452616d05290e6f65
Ark223 <Ark223Neww@gmail.com>
2020-03-16 12:21:10 +0100

    CPP

--- C++/Lab03/3.4.cpp
+++ C++/Lab03/3.4.cpp
@@ -7,7 +7,7 @@ int main() {
     double a, b, result;
     cout << "Podaj liczbe a i b: " << endl;
     cin >> a >> b;
-    cout << "Dzialania: 1.Suma\n2.Roznica\n3.Iloczyn\n4.Iloraz" << endl;
+    cout << "Dzialania: \n1.Suma\n2.Roznica\n3.Iloczyn\n4.Iloraz" << endl;
     cin >> choose;
     while (choose == 4 && b == 0.0) {
         cout << "Nie mozna dzielic przez zero! Prosze wpisac inna liczbe!" << endl;
