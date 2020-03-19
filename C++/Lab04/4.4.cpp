31df083bdca84fc764d934ed406c598cccf29c5d
Ark223 <Ark223Neww@gmail.com>
2020-03-19 14:10:26 +0100

    CPP

---
+++ C++/Lab04/4.4.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, temp, w = 0;
+    double aver;
+    vector<int> a, b;
+    cout << "Podaj ilosc liczb: " << endl;
+    cin >> n;
+    cout << "Wypisz liczby: " << endl;
+    while (a.size() < n) {
+        cin >> temp;
+        a.push_back(temp);
+    }
+    cout << "Wypisz wagi: " << endl;
+    while (b.size() < n) {
+        cin >> temp;
+        b.push_back(temp);
+    }
+    int s = a.size();
+    for (int i = 0; i < s; i++) {
+        aver += double(a[i] * b[i]);
+        w += double(b[i]);
+    }
+    aver /= w;
+    cout << "Srednia wazona liczb wynosi: " << aver << endl;
+    return 0;
+}
